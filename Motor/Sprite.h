8c9649280d7eeb1f3e2f9f37a68beb3b78e7caf6
aelvismorales <faviotio123@gmail.com>
2020-09-02 16:21:06 -0500

    Proyecto TeoriaJuegos

---
+++ Motor/Sprite.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <GL/glew.h>
+class Sprite {
+private:
+	float x;
+	float y;
+	int width;
+	int height;
+	//todo vbo vertex Buffer Object 
+public:
+	Sprite();
+	~Sprite();
+	void init(float _x, float _y, int _width, int _height);
+	void draw();
+};
